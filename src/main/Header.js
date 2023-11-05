d6034260cdf7d02a33ff84a032a61e6addf10ece
blinkEgor <barminov44@gmail.com>
2023-11-05 13:47:30 +0300

    install react-devtools

--- src/main/Header.js
+++ src/main/Header.js
@@ -3,18 +3,18 @@ import logo from '../img/logo.png';
 
 function Header() {
 
-    // const hamburger = document.getElementsByClassName(".hamburger");
-    // const navMenu = document.getElementsByClassName(".nav-menu");
+    const hamburger = document.getElementsByClassName(".hamburger");
+    const navMenu = document.getElementsByClassName(".nav-menu");
         
-    // hamburger.addEventListener('click', () => {
-    //     hamburger.classList.toggle("active");
-    //     navMenu.classList.toggle("active");
-    // });
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
 
-    // document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    //     hamburger.classList.remove("active");
-    //     navMenu.classList.remove("active");
-    // }));
+    document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
 
     return (
         <header>
